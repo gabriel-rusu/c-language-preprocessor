e6d3397273fc608e3530053db336cbdca01e6426
RusuGabriel <rusugabriel13@gmail.com>
2020-03-02 23:27:17 +0200

    update #1

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 257
 
 int main(int argc ,char **argv)
 {
+    char buffer[BUFFER_SIZE];
     
-    putc('A',stdout);
-    fflush(stdout);
+    scanf("%[^\n]s",buffer);
+    printf("%s\n",buffer);
 
     return 0;
 }
