3f6ad9c085c4d8e97c14af2a0ca323835b0ac4a1
RusuGabriel <rusugabriel13@gmail.com>
2020-03-06 22:08:54 +0200

    update

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 #define FAILED 2
 #define SYMBOL_FLAG "-D"
 #define FLAG_CHAR '-'
+#define INPUT_FILE_EXTENSION ".in"
+#define OUTPUT_FILE_EXTENSION ".out"
+#define INVALID_FILE_EXTENSION "."
 
 
 typedef struct Node{
@@ -21,32 +24,55 @@ typedef struct HashMap{
 }HashMap;
 
 int create_new(HashMap ** hashMap,int size);
-int process_arguments(char **arguments,int argument_count,HashMap *hashMap);
+int process_arguments(char **arguments,int argument_count,HashMap *hashMap,FILE **file_in,FILE **file_out);
 bool is_invalid_flag(char *argument);
 bool is_symbol(char *argument);
 void freeMap(HashMap **hashMap);
 void addSymbol(char **arguments,int index, HashMap* hashMap);
-void verify(void *pointer,int line_numer){
-    if(pointer==NULL)
-    {
-        printf("Allocation error on line %d!\n",line_numer);
-        exit(EXIT_FAILURE);
-    }
-}
+
+bool is_file(char * name,char * extension);
+void verify(void *pointer,int line_number);
+int process(FILE *file_in,FILE *file_out,HashMap *hashMap);
+
+
 int main(int argc ,char **argv)
 {
     HashMap *hashMap = NULL;
+    FILE *file_in = stdin,*file_out = stdout;
     create_new(&hashMap,argc);
-    if(process_arguments(argv,argc,hashMap) == FAILED)
+    if(process_arguments(argv,argc,hashMap,&file_in,&file_out) == FAILED)
         exit(EXIT_FAILURE);
+    process(file_in,file_out,hashMap);
     freeMap(&hashMap);
     return 0;
+}
+
+int process(FILE *file_in,FILE *file_out,HashMap *hashMap){
+    char line[256];
+    while (!fscanf(file_in,"%[^\n]s",line)!=EOF)
+    {
+        fprintf(file_out,"%s\n",line);
+    }
+    
+}
+
+bool is_file(char * name,char * extension){
+    return strstr(name,extension)!=NULL;
 }
 
 void freeMap(HashMap **hashMap){
     if(*hashMap){
         free((*hashMap)->array);
         free(*hashMap);
+        *hashMap = NULL;
+    }
+}
+
+void verify(void *pointer,int line_number){
+    if(pointer==NULL)
+    {
+        printf("Allocation error on line %d!\n",line_number);
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -59,12 +85,18 @@ int create_new(HashMap ** hashMap,int size){
     return 0;
 }
 
-int process_arguments(char **arguments,int argument_count,HashMap *hashMap){
+int process_arguments(char **arguments,int argument_count,HashMap *hashMap,FILE **file_in,FILE **file_out){
     int index = 0;
     while(index < argument_count){
         if(is_symbol(arguments[index])){
             addSymbol(arguments,index,hashMap);
         }
+        else if(is_file(arguments[index],INPUT_FILE_EXTENSION)){
+            verify(*file_in = fopen(arguments[index],"r"),__LINE__);
+        }else if(is_file(arguments[index],OUTPUT_FILE_EXTENSION)){
+            verify(*file_out = fopen(arguments[index],"w"),__LINE__);
+        }else if(is_file(arguments[index],INVALID_FILE_EXTENSION))
+            return FAILED;
         else if(is_invalid_flag(arguments[index]))
             return FAILED;
         index++;
