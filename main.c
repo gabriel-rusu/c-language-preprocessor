7944128cbff0523056e8a7727fc2086f282b9ff7
RusuGabriel <rusugabriel13@gmail.com>
2020-03-08 08:26:56 +0200

    -D flag support #2

--- main.c
+++ main.c
@@ -153,7 +153,7 @@ int process_arguments(char **arguments,int argument_count,LinkedList *linkedList
 }
 
 void addSymbol(char **arguments,int index, LinkedList* linkedList){
-    if(strstr(arguments[index],SYMBOL_FLAG)&&strlen(arguments)>2)
+    if(strstr(arguments[index],SYMBOL_FLAG)&&strlen(arguments[index])>2)
         {
             char temp[55];
             memcpy(temp,&(arguments[index][2]),strlen(&(arguments[index][2]))+1);
