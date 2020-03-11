21320980a0e5b5e1b4a449fd7dfe2d6e5bb67a50
RusuGabriel <rusugabriel13@gmail.com>
2020-03-12 00:31:29 +0200

    refactoring code part.2

--- src/utils.h
+++ src/utils.h
@@ -6,23 +6,26 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define BUFFER_SIZE 257
+
 #define FAILED 2
+#define SUCCESS 1
+#define FLAG_CHAR '-'
+#define BUFFER_SIZE 256
 #define SYMBOL_FLAG "-D"
 #define INCLUDE_FLAG "-I"
-
-#define FLAG_CHAR '-'
 #define INPUT_FILE_EXTENSION ".in"
 #define OUTPUT_FILE_EXTENSION ".out"
 #define INVALID_FILE_EXTENSION "."
-#define SUCCESS 13
+
+
+
 #define PATH_TO_INPUT "_test/inputs/"
-
-
 #define IFDEF_DIRECTIVE "#ifdef"
 #define IFNDEF_DIRECTIVE "#ifndef"
 #define INCLUDE_DIRECTIVE "#include"
 #define DEFINE_DIRECTIVE "#define"
+
+
 
 
 void verify(void *pointer, int line_number);
